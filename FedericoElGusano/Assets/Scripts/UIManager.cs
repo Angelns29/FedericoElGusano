976c910f898901b67e11f7f63eda38cd757f2d32
AngelNavarreteSanchez <angel.navarrete.7e6@itb.cat>
2023-10-24 16:53:01 +0200

    PauseMenu

--- FedericoElGusano/Assets/Scripts/UIManager.cs
+++ FedericoElGusano/Assets/Scripts/UIManager.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
     [Header("Start")]
     [SerializeField] private GameObject startMenu;
+    [Header("Pause")]
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject pauseButton;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +23,22 @@ public class UIManager : MonoBehaviour
         
     }
    
-
-
+    public void ShowPauseMenu()
+    {
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+    public void Resume()
+    {
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+    public void ReturnMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
     public void DisableStart()
     {
         startMenu.SetActive(false);
