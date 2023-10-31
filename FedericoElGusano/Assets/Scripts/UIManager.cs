f600245afb5d11190b62fe0f69c1cb1b8120aec0
AngelNavarreteSanchez <angel.navarrete.7e6@itb.cat>
2023-10-31 16:22:26 +0100

    Splash Menu

--- FedericoElGusano/Assets/Scripts/UIManager.cs
+++ FedericoElGusano/Assets/Scripts/UIManager.cs
@@ -37,7 +37,7 @@ public class UIManager : MonoBehaviour
     }
     public void ReturnMenu()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
         Time.timeScale = 1.0f;
     }
     #endregion
