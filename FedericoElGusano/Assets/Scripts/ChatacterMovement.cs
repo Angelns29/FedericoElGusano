48540ebc2784606ae49cfdee1d74a82589a7462e
Samuel-Arteaga <145785950+Samuel-Arteaga@users.noreply.github.com>
2023-11-08 19:37:33 +0400

    Merge devSamuel to dev

--- FedericoElGusano/Assets/Scripts/ChatacterMovement.cs
+++ FedericoElGusano/Assets/Scripts/ChatacterMovement.cs
@@ -14,62 +14,58 @@ public class ChatacterMovement : MonoBehaviour
     public float jumpForce = 10f;
     public float velocidad;
     private GameObject _platform;
-   
+
     void Awake()
     {
         _animator = gameObject.GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
         _sr = GetComponent<SpriteRenderer>();
 
-        _platform = GameObject.Find("Col*");
-        _collider = _platform.GetComponent<BoxCollider2D>();
+        //_platform = GameObject.Find("Col*");
+        _collider = gameObject.GetComponent<BoxCollider2D>();
 
     }
-
     // Update is called once per frame
     void Update()
     {
         MovimientoHorizontal();
         Salto();
-      
+
     }
     void MovimientoHorizontal()
     {
-        
+
 
         //_rb.velocity = new Vector2(velocidad, _rb.velocity.y);
-       
+
     }
     void Salto()
     {
-       
         //Salto
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
-            //Invoke(nameof(ActivateCollider), 0.5f); // Esto activará el collider después de 0.5 segundos.
-        }
-      
+            Invoke(nameof(ActivateCollider), 0.5f); // Esto activará el collider después de 0.5 segundos.
 
-        //Desactiva collider 
+        }
         //if (Input.GetKeyDown(KeyCode.DownArrow))
         //{
-        //    _collider.enabled = false;
+        //   DesactivaCollider();
         //}
     }
-
-    //void ActivateCollider()
-    //{
-    //    _collider.enabled = true;
-    //}
-    void CambioPlataforma()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        //Desactiva collider 
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+        if (collision.gameObject.tag=="Plataforma2"&& Input.GetKeyDown(KeyCode.DownArrow))
         {
-            _collider.enabled = false;
+            DesactivaCollider();
         }
     }
-
-
+    void ActivateCollider()
+    {
+        _collider.enabled = true;
+    }
+    void DesactivaCollider()
+    {
+        _collider.enabled = false;
+    }
 }
