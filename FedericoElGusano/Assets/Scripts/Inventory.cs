2a71d017706f457a9103bdd9fc06d530f99af0d7
Elfo <iceseam0203@gmail.com>
2023-12-05 16:01:30 +0100

    Persistencia con Tienda y inventario

---
+++ FedericoElGusano/Assets/Scripts/Inventory.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory
+{
+    public int coins;
+    public int armor;
+    public int weapon;
+    public int charge;
+
+}
